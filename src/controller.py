3b3bf292e3b053aed445d58f2cdbf45ea5b16465
goanna5 <143087684+goanna5@users.noreply.github.com>
2024-08-17 00:29:21 +1000

    historical keypress

--- src/controller.py
+++ src/controller.py
@@ -7,8 +7,8 @@ from constants import *
 class ImvimController:
     def __init__(self, master: tk.Tk) -> None:
         # create model and view instances?
-        self._invimModel = ImvimModel()
-        self._invimView = ImvimView()
+        self._imvimModel = ImvimModel()
+        self._imvimView = ImvimView()
 
         # handle key presses 
         master.bind('<KeyPress>', self.handle_keypress)
@@ -19,22 +19,26 @@ class ImvimController:
         # references methods in the view
         key_pressed = event.char.lower()
 
+        # pass keys pressed to the thing displaying on the gui
+        self._imvimView.display_keypress()
+
+
         # so far:
         if key_pressed == UP:
             # move up
-            self._invimModel.move_cursor(UP[0], UP[1])
+            self._imvimModel.move_cursor(UP[0], UP[1])
 
         elif key_pressed == DOWN:
             # move down
-            self._invimModel.move_cursor(DOWN[0], DOWN[1])
+            self._imvimModel.move_cursor(DOWN[0], DOWN[1])
 
         elif key_pressed == LEFT:
             # move left
-            self._invimModel.move_cursor(LEFT[0], LEFT[1])
+            self._imvimModel.move_cursor(LEFT[0], LEFT[1])
 
         elif key_pressed == RIGHT:
             # move right
-            self._invimModel.move_cursor(RIGHT[0], RIGHT[1])
+            self._imvimModel.move_cursor(RIGHT[0], RIGHT[1])
 
         # decide others later
 
--- src/model.py
+++ src/model.py
@@ -6,6 +6,7 @@ class ImvimModel():
         self.cursor_coords = (0,0)
         self.level = 0
         self.goal_text = []
+        self.historical_keypress = []
     
     def get_cursor_coords(self):
         # (row_num, col_num)
@@ -66,3 +67,12 @@ class ImvimModel():
         self.cursor_coords = (0,0)
         self.level += 1
         self.goal_text = GOAL_TEXTS[self.level]
+
+    def set_historical_keypress(self, keypressed):
+        if len(self.historical_keypress) > 10:
+            self.historical_keypress = self.historical_keypress[1:]
+        self.historical_keypress.append(keypressed)
+    
+    def get_historical_keypress(self):
+        return self.historical_keypress
+
