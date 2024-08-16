93966ea78d7fb36446aa84db6d267330b778afc1
Ryan Wong <ryandpmwong@gmail.com>
2024-08-17 06:43:58 +1000

    Add features.py

---
+++ src/features.py
@@ -0,0 +1,47 @@
+def handle_back_and_del(input, model, other):
+    """
+    EASY
+    If input is backspace or delete,
+    remove entire row
+    """
+    pass
+
+def arrow_to_char(input, model, other):
+    """
+    EASY
+    If input is an arrow key, write corresponding letter
+    """
+    pass
+
+def char_to_arrow(input, model, other):
+    """
+    NOT EASY
+    If input is "udlr" convert this to a direction
+    """
+    pass
+
+def handle_spacebar(input, model, other):
+    """
+    EASY
+    Write "space" if the input is a spacebar
+    """
+    pass
+
+def caeser_cipher(input, model, other):
+    """
+    caesar cipher shift input if it is a letter
+    ignore other characters.
+    """
+    pass
+
+def convert_space(input, model, other):
+    """
+    HARD
+    If we have just created the word "space" *ALL LOWERCASE*
+    then replace "space" with " "
+    Important: should do nothing if the word "space" was made
+    due to a spacebar press
+    (when the user presses the spacebar it prints "space" but
+    this shouldnt immediately turn back into a " ")
+    """
+    pass
